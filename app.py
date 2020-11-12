50195917af9929e372ee51e0b5786f1450d0729f
Andrey <misael.albanil@vmlyr.com>
2020-11-12 02:33:47 -0500

    model

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ Created on Fri Oct  9 01:07:31 2020
 import mysql.connector
 import time
 from trello import TrelloClient
+import json
 import sys
 import os
 
@@ -26,14 +27,15 @@ RDSUSER = "adminarpic"
 RDSPASS = "adminarpic$2020"
 RDSDB = "arpic"
 
-LIMITCARDS = 3
+LIMITCARDS = 1
 CRON_TIME = 20
 
 
 
 def arpicAddcard(obj_labels,obj_list,data):
     label_to_add = next(x for x in obj_labels if x.name == str(data[2]))
-    obj_list.add_card(data[0],data[1],[label_to_add]) 
+    obj_list.add_card(str(data[0]),str(data[1]),[label_to_add]) 
+   # obj_list.add_card(data[0],data[1],[label_to_add]) 
 
 def arpicGetLastRdsTrelloCard(rds_cursor):
     query = ("SELECT value FROM arpic.arpic_parameters where parameter = 'rds_trello_card' limit 1")
@@ -53,7 +55,56 @@ def arpicGetRdsMessages(rds_cursor,last_id_card):
     rds_cursor.execute(query)
     rst = rds_cursor.fetchall()
     return rst
+
+def arpicFriendlyTitle(obj_title,label):
+    ct = json.loads(obj_title)
+    if label == 'contact_form':
+        return ct['email']
+    elif label == 'twitter': 
+        return "@"+ct['account']
+    elif label == 'survey': 
+        return ct['email']      
+    else:
+        return obj_title
+
+def arpicFriendlyBody(obj_title,obj_body,label):
+    ct = json.loads(obj_title)
+    if label == 'contact_form':
+        ds_description = "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" % \
+            ("CONTACTO",
+            "===",
+            "***Nombre:*** "+ct['name'],
+            "***Email:*** "+ct['email'],
+            "***Telefono:*** "+ct['phone'],
+            "MENSAJE",
+            "===",
+            obj_body)  
+    elif label == 'twitter':   
+        ds_description = "%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n" % \
+            ("CONTACTO",
+            "===",
+            "***Nombre:*** "+ct['name'],
+            "***Cuenta:*** @"+ct['account'],
+            "***Id:*** "+str(ct['id']),
+            "***Seguidores:*** "+str(ct['followers_count']),
+            "***Amigos:*** "+str(ct['friends_count']),
+            "MENSAJE",
+            "===",
+            obj_body)
+    if label == 'survey':
+        ds_description = "%s\n%s\n%s\n%s\n%s\n%s\n" % \
+            ("CONTACTO",
+            "===",
+            "***Email:*** "+ct['email'],
+            "AUDIO",
+            "===",
+            obj_body)              
+    else:
+        ds_description = "%s" % \
+            ("SIN NADA")
     
+    return ds_description
+
     
 def main():   
     #RDS flow
@@ -66,6 +117,7 @@ def main():
     cursor = mydb.cursor()
     lastCard = arpicGetLastRdsTrelloCard(cursor)
     rdsMesagges = arpicGetRdsMessages(cursor,lastCard)
+
     #...................................................  
     #trello flow
     client = TrelloClient(
@@ -90,22 +142,25 @@ def main():
         rdsCardId = 0
         for x in rdsMesagges:
             rdsCardId = int(x[0])
-            data = [x[3].decode(),x[4].decode(),x[2].decode()]
+            rdsTitle = arpicFriendlyTitle(x[3],x[2])
+            rdsDescription = arpicFriendlyBody(x[3],x[4],x[2])
+            data = [rdsTitle,rdsDescription,x[2]]
             arpicAddcard(TLABELS,TLIST_OBJ,data)
             print("- Add trello card:" + str(rdsCardId) )
             
-        updlastCard = arpicSetLastRdsTrelloCard(mydb,rdsCardId)
+        #arpicSetLastRdsTrelloCard(mydb,rdsCardId)
     else:
         print("- Not new messages.")
    
     
 
 if __name__ == '__main__':
-    while True:
-        lc_start = os.environ['ARP_START']
-        #lc_start = 1
-        if lc_start == 1:
-            main()
-            time.sleep(CRON_TIME)
-            print("Scanning RDS messages V2...")
+    main()
+    #while True:
+        #lc_start = os.environ['ARP_START']
+     #   lc_start = 1
+     #   if lc_start == 1:
+     #       main()
+            #time.sleep(CRON_TIME)
+    #        print("Scanning RDS messages V2...")
     
